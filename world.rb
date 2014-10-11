f1bc31cffbb8541aaf1632f3a8a685dec705f673
Hazen Johnson <leyarotheconquerer@yahoo.com>
2014-10-11 01:04:29 -0500

    Add camera movement to world.

--- world.rb
+++ world.rb
@@ -9,10 +9,54 @@ class World
         @worldSprites = Sprites::Group.new
         @background = Surface.load "assets/images/starfieldTileable.png"
         @cameraPosition = [0,0]
-        puts "Hi there!"
+        @backgroundPosition = @cameraPosition
+    end
+
+    def moveCamera(deltaVec)
+        @cameraPosition[0] += deltaVec[0]
+        @cameraPosition[1] += deltaVec[1]
+
+        # Magical modulus making infinite starfields... woot!
+        @backgroundPosition[0] = @cameraPosition[0] % @background.width
+        @backgroundPosition[1] = @cameraPosition[1] % @background.height
+
+        @cameraPosition
+    end
+
+    def absoluteMoveCamera(positionVec)
+        @cameraPosition = positionVec
+
+        # Magical modulus making infinite starfields... woot!
+        @backgroundPosition[0] = @cameraPosition[0] % @background.width
+        @backgroundPosition[1] = @cameraPosition[1] % @background.height
+
+        @cameraPosition
+    end
+
+    def cameraPosition()
+        @cameraPosition
     end
 
     def draw(screen)
-        @background.blit screen, @cameraPosition
+        # Draw the central background
+        @background.blit screen, @backgroundPosition
+
+        # Draw adjacent backgrounds
+        @background.blit screen,
+            [@backgroundPosition[0] - @background.width, @backgroundPosition[1]]
+        @background.blit screen,
+            [@backgroundPosition[0] - @background.width, @backgroundPosition[1] + @background.height]
+        @background.blit screen,
+            [@backgroundPosition[0] - @background.width, @backgroundPosition[1] - @background.height]
+        @background.blit screen,
+            [@backgroundPosition[0], @backgroundPosition[1] + @background.height]
+        @background.blit screen,
+            [@backgroundPosition[0], @backgroundPosition[1] - @background.height]
+        @background.blit screen,
+            [@backgroundPosition[0] + @background.width, @backgroundPosition[1] + @background.height]
+        @background.blit screen,
+            [@backgroundPosition[0] + @background.width, @backgroundPosition[1] - @background.height]
+        @background.blit screen,
+            [@backgroundPosition[0] + @background.width, @backgroundPosition[1]]
     end
 end
