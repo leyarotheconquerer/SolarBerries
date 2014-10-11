4e3c6b973cd26ae141735945309ee8116476d6ec
SpenSer Bray <spenser.m.bray@gmail.com>
2014-10-10 23:26:43 -0700

    Fix ship class to properly specify position

--- ship.rb
+++ ship.rb
@@ -8,7 +8,7 @@ class Ship
   def initialize()
     @shipSprites = Sprites::Group.new
     @shipSurface = Surface.load "assets/images/ship.png"
-    @location = Vector[0, 0]
+    @location = [100, 100]
   end
 
   def draw(screen)
