9ef82f458623e1982838ac63fc55b935e0e5dcf9
SpenSer Bray <spenser.m.bray@gmail.com>
2014-10-10 22:40:15 -0700

    Add ship.rb w/ initial setup

---
+++ ship.rb
@@ -0,0 +1,17 @@
+require "rubygems"
+require "rubygame"
+require "matrix"
+
+include Rubygame
+
+class Ship
+  def initialize()
+    @shipSprites = Sprites::Group.new
+    @shipSurface = Surface.load "assets/images/ship.png"
+    @location = Vector[0, 0]
+  end
+
+  def draw(screen)
+    @shipSurface.blit screen, @location
+  end
+end
